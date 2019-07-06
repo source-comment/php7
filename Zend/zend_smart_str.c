fb94c50e3ab3fca6f4b116b7799f896f5ea28927
wuzhijian <wuzj@vistopia.com.cn>
2019-07-06 15:23:30 +0800

    add doc to Zend/zend_smart

--- Zend/zend_smart_str.c
+++ Zend/zend_smart_str.c
@@ -32,29 +32,45 @@
 #define SMART_STR_NEW_SIZE(len) \
 	(((len + SMART_STR_OVERHEAD + SMART_STR_PAGE) & ~(SMART_STR_PAGE - 1)) - SMART_STR_OVERHEAD - 1)
 
+/**
+ * @description: 向内存池为smart_str申请内存
+ * @param smart_str* str 指针
+ * @param size_t len 长度
+ * @return: void
+ */
 ZEND_API void ZEND_FASTCALL smart_str_erealloc(smart_str *str, size_t len)
 {
-	if (UNEXPECTED(!str->s)) {
+	if (UNEXPECTED(!str->s)) {	
+		//实际值为空，则新申请内存
 		str->a = len < SMART_STR_START_SIZE
 				? SMART_STR_START_SIZE
 				: SMART_STR_NEW_SIZE(len);
 		str->s = zend_string_alloc(str->a, 0);
 		ZSTR_LEN(str->s) = 0;
 	} else {
+		//如果已经有值，则扩展内存
 		str->a = SMART_STR_NEW_SIZE(len);
 		str->s = (zend_string *) erealloc2(str->s, _ZSTR_HEADER_SIZE + str->a + 1, _ZSTR_HEADER_SIZE + ZSTR_LEN(str->s) + 1);
 	}
 }
 
+/**
+ * @description: 向系统为smart_str申请内存
+ * @param smart_str* str 指针
+ * @param size_t len 长度
+ * @return: void
+ */
 ZEND_API void ZEND_FASTCALL smart_str_realloc(smart_str *str, size_t len)
 {
 	if (UNEXPECTED(!str->s)) {
+		//实际值为空，则新申请内存
 		str->a = len < SMART_STR_START_SIZE
 				? SMART_STR_START_SIZE
 				: SMART_STR_NEW_SIZE(len);
 		str->s = zend_string_alloc(str->a, 1);
 		ZSTR_LEN(str->s) = 0;
 	} else {
+		//如果已经有值，则扩展内存
 		str->a = SMART_STR_NEW_SIZE(len);
 		str->s = (zend_string *) realloc(str->s, _ZSTR_HEADER_SIZE + str->a + 1);
 	}
@@ -65,20 +81,34 @@ ZEND_API void ZEND_FASTCALL smart_str_realloc(smart_str *str, size_t len)
 #define VK_ESCAPE '\e'
 #endif
 
+/**
+ * @description: 计算字符串的长度，包含\r,\n,\f,\t,\v,\\,\e,以及ASCII表中的特殊字符
+ * @param char* s 字符串
+ * @param size_t l 字符串本身长度，不包含转移和特殊字符
+ * @return: size_t
+ */
 static size_t zend_compute_escaped_string_len(const char *s, size_t l) {
 	size_t i, len = l;
 	for (i = 0; i < l; ++i) {
 		char c = s[i];
 		if (c == '\n' || c == '\r' || c == '\t' ||
 			c == '\f' || c == '\v' || c == '\\' || c == VK_ESCAPE) {
-			len += 1;
+			len += 1;	//转移字符1个长度
 		} else if (c < 32 || c > 126) {
+			//特殊字符占用3长度
 			len += 3;
 		}
 	}
 	return len;
 }
 
+/**
+ * @description: 追加特殊字或转移字符到smart_str末尾
+ * @param smart_str * str smart_str指针地址
+ * @param char* s 追加字符串
+ * @param size_t l 追加字符串长度
+ * @return: void
+ */
 ZEND_API void ZEND_FASTCALL smart_str_append_escaped(smart_str *str, const char *s, size_t l) {
 	char *res;
 	size_t i, len = zend_compute_escaped_string_len(s, l);
@@ -118,6 +148,13 @@ ZEND_API void ZEND_FASTCALL smart_str_append_escaped(smart_str *str, const char 
 	}
 }
 
+/**
+ * @description: 打印smart_str
+ * @param smart_str* dest smart_str指针
+ * @param char* format 格式化
+ * @param ....参数列表
+ * @return: 
+ */
 ZEND_API void ZEND_FASTCALL smart_str_append_printf(smart_str *dest, const char *format, ...) {
 	va_list arg;
 	va_start(arg, format);
--- Zend/zend_smart_str.h
+++ Zend/zend_smart_str.h
@@ -52,11 +52,19 @@ ZEND_API void ZEND_FASTCALL smart_str_append_printf(smart_str *dest, const char 
 
 END_EXTERN_C()
 
+/**
+ * @description: 为smart_str开辟内存空间
+ * @param smart_str *str 指针地址
+ * @param size_t len 字符串长度
+ * @param zend_bool persistent 是否不用内存池
+ * @return: size_t
+ */
 static zend_always_inline size_t smart_str_alloc(smart_str *str, size_t len, zend_bool persistent) {
-	if (UNEXPECTED(!str->s)) {
+	if (UNEXPECTED(!str->s)) {	//如果字符串为空，则直接申请内存
 		goto do_smart_str_realloc;
 	} else {
 		len += ZSTR_LEN(str->s);
+		//如果新长度加上字符串长度，大于或等于smart_str的长度，则新申请内存
 		if (UNEXPECTED(len >= str->a)) {
 do_smart_str_realloc:
 			if (persistent) {
@@ -69,6 +77,11 @@ do_smart_str_realloc:
 	return len;
 }
 
+/**
+ * @description: 释放smart_str
+ * @param smart_str* str smart_str指针
+ * @return: void
+ */
 static zend_always_inline void smart_str_free(smart_str *str) {
 	if (str->s) {
 		zend_string_release(str->s);
@@ -77,16 +90,31 @@ static zend_always_inline void smart_str_free(smart_str *str) {
 	str->a = 0;
 }
 
+/**
+ * @description: 在字符串内容末尾追加\0
+ * @param smart_str* str smart_str指针
+ * @return: void
+ */
 static zend_always_inline void smart_str_0(smart_str *str) {
 	if (str->s) {
 		ZSTR_VAL(str->s)[ZSTR_LEN(str->s)] = '\0';
 	}
 }
 
+/**
+ * @description: 获取字符串内容长度
+ * @param smart_str* str smart_str指针
+ * @return: size_t
+ */
 static zend_always_inline size_t smart_str_get_len(smart_str *str) {
 	return str->s ? ZSTR_LEN(str->s) : 0;
 }
 
+/**
+ * @description: 提取smart_str中的zend_string
+ * @param smart_str* str smart_str指针
+ * @return: zend_string*
+ */
 static zend_always_inline zend_string *smart_str_extract(smart_str *str) {
 	if (str->s) {
 		zend_string *res;
@@ -99,40 +127,90 @@ static zend_always_inline zend_string *smart_str_extract(smart_str *str) {
 	}
 }
 
+/**
+ * @description: 追加一个字符到smart_str的zend_string末尾
+ * @param smart_str* smart_str 指针
+ * @param char ch 追加的的字符
+ * @param zend_bool persistent 是否不用内存池
+ * @return: 
+ */
 static zend_always_inline void smart_str_appendc_ex(smart_str *dest, char ch, zend_bool persistent) {
 	size_t new_len = smart_str_alloc(dest, 1, persistent);
 	ZSTR_VAL(dest->s)[new_len - 1] = ch;
 	ZSTR_LEN(dest->s) = new_len;
 }
 
+/**
+ * @description: 追加字符串到smart_str的zend_string末尾
+ * @param smart_str* smart_str 指针
+ * @param char* str 追加的的字符串
+ * @param size_t len 追加字符串长度
+ * @param zend_bool persistent 是否不用内存池
+ * @return: void
+ */
 static zend_always_inline void smart_str_appendl_ex(smart_str *dest, const char *str, size_t len, zend_bool persistent) {
 	size_t new_len = smart_str_alloc(dest, len, persistent);
 	memcpy(ZSTR_VAL(dest->s) + ZSTR_LEN(dest->s), str, len);
 	ZSTR_LEN(dest->s) = new_len;
 }
 
+/**
+ * @description: 追加字符串到smart_str的zend_string末尾
+ * @param smart_str* smart_str 指针
+ * @param zend_string* src 追加的的字符串结构指针
+ * @param zend_bool persistent 是否不用内存池
+ * @return: void
+ */
 static zend_always_inline void smart_str_append_ex(smart_str *dest, const zend_string *src, zend_bool persistent) {
 	smart_str_appendl_ex(dest, ZSTR_VAL(src), ZSTR_LEN(src), persistent);
 }
 
+/**
+ * @description: 追加smart_str到smart_str的末尾
+ * @param smart_str* smart_str 指针
+ * @param zend_string* src 追加的的字符串结构指针
+ * @param zend_bool persistent 是否不用内存池
+ * @return: void
+ */
 static zend_always_inline void smart_str_append_smart_str_ex(smart_str *dest, const smart_str *src, zend_bool persistent) {
 	if (src->s && ZSTR_LEN(src->s)) {
 		smart_str_append_ex(dest, src->s, persistent);
 	}
 }
 
+/**
+ * @description: 追加一个整数到smart_str的末尾
+ * @param smart_str* smart_str 指针
+ * @param zend_long num 整数值
+ * @param zend_bool persistent 是否不用内存池
+ * @return: void
+ */
 static zend_always_inline void smart_str_append_long_ex(smart_str *dest, zend_long num, zend_bool persistent) {
 	char buf[32];
 	char *result = zend_print_long_to_buf(buf + sizeof(buf) - 1, num);
 	smart_str_appendl_ex(dest, result, buf + sizeof(buf) - 1 - result, persistent);
 }
 
+/**
+ * @description: 追加一个正整数到smart_str的末尾
+ * @param smart_str* smart_str 指针
+ * @param zend_long num 正整数值
+ * @param zend_bool persistent 是否不用内存池
+ * @return: void
+ */
 static zend_always_inline void smart_str_append_unsigned_ex(smart_str *dest, zend_ulong num, zend_bool persistent) {
 	char buf[32];
 	char *result = zend_print_ulong_to_buf(buf + sizeof(buf) - 1, num);
 	smart_str_appendl_ex(dest, result, buf + sizeof(buf) - 1 - result, persistent);
 }
 
+/**
+ * @description: 重置smart_str的内容
+ * @param smart_str* smart_str 指针
+ * @param char* src 新字符串
+ * @param size_t len 新字符串长度
+ * @return: void
+ */
 static zend_always_inline void smart_str_setl(smart_str *dest, const char *src, size_t len) {
 	smart_str_free(dest);
 	smart_str_appendl(dest, src, len);
